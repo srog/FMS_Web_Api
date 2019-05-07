ce914f836611bd18d7336cc99c18d2a1bfb59a17
Simon Rogers <s.rogers@waxdigital.com>
2019-05-07 10:26:35 +0100

    change player attributes data structure

---
+++ Fms_Web_Api/Data/PlayerAttributeQuery.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Fms_Web_Api.Models;
+
+namespace Fms_Web_Api.Data
+{
+    public class PlayerAttributeQuery : Query
+    {
+        private const string GET = "spGetPlayerAttribute";
+        private const string GET_FOR_PLAYER = "spGetPlayerAttributes";
+        private const string INSERT = "spInsertPlayerAttribute";
+        private const string UPDATE = "spUpdatePlayerAttribute";
+
+        public IEnumerable<PlayerAttribute> GetByPlayer(int playerId)
+        {
+            return GetAllById<PlayerAttribute>(GET_FOR_PLAYER, "playerId", playerId);
+        }
+
+        public PlayerAttribute Get(int id)
+        {
+            return GetSingleById<PlayerAttribute>(GET, "playerId", id);
+        }
+
+
+
+        public int Add(PlayerAttribute playerAttribute)
+        {
+            return Add(INSERT, new Dictionary<string, object>
+                {
+                    {"playerId", playerAttribute.PlayerId },
+                    {"attributeId", playerAttribute.AttributeId },
+                    {"attributeValue", playerAttribute.AttributeValue }
+
+                });
+
+        }
+        public int Update(PlayerAttribute playerAttribute)
+        {
+            return Update(UPDATE, new { playerAttribute.Id, playerAttribute.AttributeValue });
+        }
+
+    }
+
+
+}
---
+++ Fms_Web_Api/Enums/PlayerAttributeEnum.cs
@@ -0,0 +1,20 @@
+﻿namespace Fms_Web_Api.Enums
+{
+    public enum PlayerAttributeEnum
+    {
+        Speed = 1,
+        Shooting = 2,
+        Crossing = 3,
+        Passing = 4,
+        Skills = 5,
+        Tackling = 6,
+        Defending = 7,
+        Handling = 8,
+        Aggression = 9,
+        Form = 10,
+        Morale = 11,
+        Happiness = 12,
+        Stamina = 13,
+        Strength = 14
+    }
+}
